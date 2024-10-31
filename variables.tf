88d831caa9673f910b37432d4a38b616a12e1874
ArturZimbarg <86981718+ArturZimbarg@users.noreply.github.com>
2024-10-31 17:31:18 -0300

    projeto de cloud terraform

--- variables.tf
+++ variables.tf
@@ -46,25 +46,3 @@ variable "lambda_function_name" {
   type        = string
   default     = "PollyTextToSpeechFunction"
 }
-outputs.tf
-Esse arquivo outputs.tf exibe saídas importantes, como o ARN da tabela DynamoDB, o nome do bucket S3 e o endpoint IoT para facilitar o acesso a esses recursos após a criação.
-
-hcl
-Copiar código
-# Exibe o nome do bucket S3 para dados
-output "s3_bucket_name" {
-  description = "Nome do bucket S3 para armazenamento de dados"
-  value       = aws_s3_bucket.data_bucket.id
-}
-
-# Exibe o ARN da tabela DynamoDB
-output "dynamodb_table_arn" {
-  description = "ARN da tabela DynamoDB para armazenamento das rotas"
-  value       = aws_dynamodb_table.route_data_table.arn
-}
-
-# Exibe o endpoint IoT
-output "iot_endpoint" {
-  description = "Endpoint para comunicação IoT"
-  value       = data.aws_iot_endpoint.iot_endpoint.endpoint_address
-}
